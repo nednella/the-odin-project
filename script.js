cd2651ec4030abadd8b6cb90ac15e68ecfe539ac
nednella <119449811+nednella@users.noreply.github.com>
2023-07-21 20:09:07 +0100

    added game functionality: colour grab/fill, shade/bright modes

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var currentMode = DEFAULT_MODE              // default: 'draw' ----- options: 'g
 var currentSize = DEFAULT_SIZE
 
 
+
 // variable updaters and helper functions
 function updateCurrentColour(newColour) {
     currentColour = newColour
@@ -31,6 +32,26 @@ function updateCurrentSize(newSize) {
     currentSize = newSize
 }
 
+function splitRgb(rgb) {
+    rgb = rgb.replace(/^\D+|\D+$/g, "")     // replaces "rgb(X, Y, Z)" with "X, Y, Z"
+    rgb = rgb.split(",")                    // turns "X, Y, Z" into an array of [X, Y, Z]
+    return rgb 
+}
+
+function rgbToHEX(rgb) {
+    rgb = splitRgb(rgb)                     // requires variable 'rgb' to be an array
+    return "#" + (1 << 24 | rgb[0] << 16 | rgb[1] << 8 | rgb[2]).toString(16).slice(1)
+}
+
+function elementAdjust(rgb, adjustConstant) {
+    rgb[0] = (parseInt(rgb[0]) + adjustConstant)
+    rgb[1] = (parseInt(rgb[1]) + adjustConstant)
+    rgb[2] = (parseInt(rgb[2]) + adjustConstant)
+    rgb = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+    return rgb
+}
+
+
 
 // DOM nodes
 // titles
@@ -39,10 +60,10 @@ const gridSize = document.getElementById('gridSize')                    // compl
 const selectorColour = document.getElementById('colourSelector')        // complete
 const selectorSize = document.getElementById('sizeSelector')            // complete
 // modes
-const btnGrab = document.getElementById('btn-grab')                     //
-const btnFill = document.getElementById('btn-fill')                     //
-const btnShade = document.getElementById('btn-shade')                   //
-const btnBright = document.getElementById('btn-bright')                 //
+const btnGrab = document.getElementById('btn-grab')                     // complete
+const btnFill = document.getElementById('btn-fill')                     // complete
+const btnShade = document.getElementById('btn-shade')                   // complete
+const btnBright = document.getElementById('btn-bright')                 // complete
 const btnErase = document.getElementById('btn-erase')                   // complete
 // misc
 const btnGridlines = document.getElementById('btn-gridlines')           // complete
@@ -55,9 +76,15 @@ const btnReset = document.getElementById('btn-reset')                   // compl
 selectorColour.oninput = (e) => updateCurrentColour(e.target.value)     // complete
 selectorSize.onmousemove = (e) => updateGridSizeUI(e.target.value)      // complete
 selectorSize.onchange = (e) => updateGrid(e.target.value)               // complete
+// modes
+btnGrab.onclick = (e) => updateCurrentMode(btnGrab.value)               // complete
+btnFill.onclick = (e) => updateCurrentMode(btnFill.value)               // complete
+btnShade.onclick = (e) => updateCurrentMode(btnShade.value)             // complete
+btnBright.onclick = (e) => updateCurrentMode(btnBright.value)           // complete
+btnErase.onclick = (e) => updateCurrentMode(btnErase.value)             // complete
 // misc
 btnGridlines.onclick = () => toggleGridlines()                          // complete
-btnReset.onclick = () => resetGrid()                                    // complete - mousedown is more responsive for larger grids
+btnReset.onclick = () => resetGrid()                                    // complete
 
 
 
@@ -128,13 +155,31 @@ function drawClick(e) {
     if (currentMode === DEFAULT_MODE) {
         e.target.style.backgroundColor = currentColour
     } else if (currentMode === 'grab') {
-        return
+        if (e.target.style.backgroundColor === '') {    // ensures a colour is grabbed when cell is blank
+            updateCurrentColour(BG_COLOUR)
+            selectorColour.value = currentColour
+        } else {
+            updateCurrentColour(e.target.style.backgroundColor)
+            selectorColour.value = rgbToHEX(currentColour)    
+        }
+        updateCurrentMode(DEFAULT_MODE)
     } else if (currentMode === 'fill') {
-        return
+        const gridElements = document.querySelectorAll('.gridElement')
+        gridElements.forEach(e => e.style.backgroundColor = currentColour)
+        updateCurrentMode(DEFAULT_MODE)
     } else if (currentMode === 'shade') {
-        return
+        if (e.target.style.backgroundColor === 'rgb(0, 0, 0)') return
+        else if (e.target.style.backgroundColor === '') {
+            e.target.style.backgroundColor = BG_COLOUR     
+        }
+        adjustConstant = -25    // negative adjust to darken
+        adjustedColour = elementAdjust(splitRgb(e.target.style.backgroundColor), adjustConstant)
+        e.target.style.backgroundColor = adjustedColour
     } else if (currentMode === 'bright') {
-        return
+        if (e.target.style.backgroundColor === 'rgb(255, 255, 255)' || e.target.style.backgroundColor === '') return
+        adjustConstant = 25     // positive adjust to brighten
+        adjustedColour = elementAdjust(splitRgb(e.target.style.backgroundColor), adjustConstant)
+        e.target.style.backgroundColor = adjustedColour
     } else if (currentMode === 'erase') {
         e.target.style.backgroundColor = ''
     } else return
@@ -143,3 +188,16 @@ function drawClick(e) {
 window.onload = () => {
     createGrid(DEFAULT_SIZE)
 }
+
+// test img pikachu 51x51, 41 wide 49 tall, 51x51 and add 5 extra to each side, 2nd col 1 black spot far left ref point, 26 from bottom on 51x51
+// rgb colours
+// red 255 0 0
+// yellow 255 255 0
+// pink 255 153 153
+// grey 192 192 192
+// brown 153 51 0
+// yellow shadow 255 204 0
+// yellow highlight 255 255 102
+
+
+
