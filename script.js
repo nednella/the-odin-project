7f38d3c4b5a9bec38d5a4dbd3be5dfb4b979aaa8
nednella <119449811+nednella@users.noreply.github.com>
2023-10-11 00:04:56 +0100

    Fix JS form validation

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function toTitleCase(str) {
 function validateForm(formSelector) {
     const formElement = document.querySelector(formSelector)
 
+    formElement.setAttribute('novalidate', '')                          // disable built-in HTML5 form validation
+
     const validationOptions = [
         {
             attribute: 'pattern',
@@ -20,26 +22,23 @@ function validateForm(formSelector) {
                 return patternRegex.test(input.value)
             },
             errorMsg: (input, label) => `Not a valid ${toTitleCase(label.textContent)}`,
-
         },
         {
             attribute: 'custommaxlength',
             isValid: input => input.value && input.value.length <= parseInt(input.getAttribute('custommaxlength')),
             errorMsg: (input, label) => `${toTitleCase(label.textContent)} needs to be less than or equal to ${input.getAttribute('custommaxlength')} characters`,
-
         },
         {
             attribute: 'minlength',
             isValid: input => input.value && input.value.length >= parseInt(input.minLength),
             errorMsg: (input, label) => `${toTitleCase(label.textContent)} needs to be at least ${input.minLength} characters`,
-
         },
         {
             attribute: 'match',
             isValid: input => {
                 const matchSelector = input.getAttribute('match')
                 const matchedElement = formElement.querySelector(`#${matchSelector}`)
-                return matchedElement && matchedElement.value.trim() == input.value.trim
+                return matchedElement && matchedElement.value.trim() === input.value.trim()
             },
             errorMsg: (input, label) => `Passwords must match`,
         },
@@ -61,31 +60,31 @@ function validateForm(formSelector) {
             // 'required' attribute
             if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
                 input.classList.add('invalid')
-                input.classList.remove('valid') // maybe remove this feature
+                input.classList.remove('valid')
                 error.textContent = option.errorMsg(input, label)
                 inputError = true
             } 
             if (inputError == false) {
                 input.classList.remove('invalid')
-                input.classList.add('valid') // maybe remove this feature
+                input.classList.add('valid')
                 error.textContent = ''
             }
         }
+        return !inputError
     }
-
-    formElement.setAttribute('novalidate', '')                      // disable built-in HTML5 form validation
-
-    formElement.addEventListener('submit', e => {                   // on form submission,  
-        e.preventDefault()                                              // disable default submit behaviour of register button
-        validateFormInputs(formElement)                                 // validate all input fields
-    })
 
     function validateFormInputs(formToValidate) {                   
         const formInputs = Array.from(formToValidate.querySelectorAll('.formInput'))
-        formInputs.forEach(formInput => {                           // loop through array and validate each input
-            validateInput(formInput)
-        })
+
+        return formInputs.every(formInput => validateInput(formInput))  // loop through array and return T/F for each input
     }
+
+    formElement.addEventListener('submit', e => {
+        e.preventDefault()
+        const isFormValid = validateFormInputs(formElement)             // check if all input vields are valid
+        if (!isFormValid) console.log("Form is NOT valid.")
+        else console.log("Form is valid.")
+    })
 }
 
 validateForm('#signupForm')
