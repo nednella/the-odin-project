50d5b4c8f6f8a30cf0a6bfdd7dec608794f5f5b9
nednella <119449811+nednella@users.noreply.github.com>
2023-11-16 03:31:12 +0000

    Add 'add book' modal and styling

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // JavaScript
 
 
+
+// Modal
+const modal = document.getElementById('modal')
+const modalOpen = document.getElementById('modal-open')
+const modalClose = document.getElementById('modal-close')
+const addBookSubmit = document.getElementById('add-book-submit')
+
+modalOpen.addEventListener('click', () => {
+    modal.showModal()
+})
+modalClose.addEventListener('click', () => {
+    modal.close()
+})
+modal.addEventListener('click', (e) => {
+    if (e.target.nodeName == 'DIALOG') {
+        modal.close()
+    }
+})
