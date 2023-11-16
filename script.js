c271c9657d5e047323b072371216df720f1dea43
nednella <119449811+nednella@users.noreply.github.com>
2023-11-16 03:36:34 +0000

    Add JS library logic - object constructor and data storage

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // JavaScript
 
 
-
 // Modal
 const modal = document.getElementById('modal')
 const modalOpen = document.getElementById('modal-open')
@@ -19,3 +18,34 @@ modal.addEventListener('click', (e) => {
         modal.close()
     }
 })
+
+
+
+// Library
+const tableBody = document.getElementById('libraryTable').getElementsByTagName('tbody')[0]
+const myLibrary = [
+    {
+        title: 'Harry Potter',
+        author: 'J. K. Rowling',
+        pages: '300',
+        read: true
+    },
+    {
+        title: 'Lord of the Rings',
+        author: 'J. R. R. Tolkien',
+        pages: '900',
+        read: false
+    }
+]
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook)
+}
