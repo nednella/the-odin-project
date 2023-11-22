7bcae81ad3ed3421c84970a69fa5b539741eec29
nednella <119449811+nednella@users.noreply.github.com>
2023-11-22 23:43:38 +0000

    Add fully functional Tic Tac Toe game within browser console

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const game = (() => {
 
-    let gameBoard = Array(9).fill('?')
+    let gameBoard = Array(9).fill(undefined)
     /* 
         0 1 2
         3 4 5
@@ -44,26 +44,17 @@ const game = (() => {
         console.log( "" );
     }
 
-    const resetGameBoard = () => gameBoard.forEach((element, index) => gameBoard[index] = '!')
-    const setGameBoard = (i, sign) => gameBoard[i - 1] = sign
-    const getGameBoard = (i) => gameBoard [i - 1]
-
-    const checkWin = (sign) => {
-        // TODO: Add check for win functionality
-    }
-    const checkDraw = (sign) => {
-        // TODO: Add check for draw functionality
-    }
-
-    // TODO: Insert helper functions here
+    const resetGameBoard = () => gameBoard.forEach((element, index) => gameBoard[index] = undefined)
+    const setGameBoard = (i, sign) => gameBoard[i] = sign
+    const getGameBoard = () => gameBoard
+    const getGameBoardAtIndex = (i) => gameBoard[i]
 
     return {
         displayGameBoard,
         resetGameBoard,
         setGameBoard,
         getGameBoard,
-        checkWin,
-        checkDraw,
+        getGameBoardAtIndex,
     }
 })()
 
@@ -86,30 +77,124 @@ const player = (sign, isCurrent) => {
 
 const controller = (() => {
 
-    // Public Functions
-    const newGame = () => {
-        do {
-            playRound()
-        } while (!game.isGameOver())
-    }
-    const resetGame = () => {
-        // TODO: Add reset functionality
-    }
-
-    // Private Variables
-    const welcomeMsg = 'Welcome to JS Tic Tac Toe (Browser Console Version)'
     const playerX = player('X', false)
     const playerO = player('O', false)
-    let currentSign = null
+    let currentSign
+    let isGameOver
+    let gameResult
 
-    // Private Functions
-    const welcome = () => console.log(welcomeMsg)
-
-    const playRound =() => {
-        // TODO: Add play a round functionality
+    const newGame = () => {
+        welcome()
+        do {
+            playRound()
+        } while (!isGameOver)
+        gameOver()
+    }
+    const resetGame = () => {
+        currentSign = ''
+        isGameOver  = ''
+        gameResult = ''
+        game.resetGameBoard()
     }
 
-    // TODO: Insert helper functions here
+    const welcomeMsg = 'Welcome to JS Tic Tac Toe (Browser Console Version)' // Temporary for Console Version
+    const welcome = () => console.log(welcomeMsg) // Temporary for Console Version
+
+    const playRound = () => {
+        game.displayGameBoard()
+        gameLogic.newTurn()
+        gameLogic.promptMove()
+        gameLogic.handleMove()
+        checkBoard.checkWin(currentSign)
+        checkBoard.checkDraw()
+    }
+    const gameOver = () => {
+        game.displayGameBoard()
+        let tmp = `Game Over! `
+        if (gameResult == 'Win') {
+            tmp += `${currentSign} Wins.`
+        } else {
+            tmp += `It's a Tie.`
+        }
+        console.log(tmp)
+    }
+
+    const gameLogic = (() => {
+        const newTurn = () => {
+            if (playerX.isCurrentPlayer() == false && playerO.isCurrentPlayer() == false) {
+                playerX.setCurrentPlayer() 
+            } else if (playerX.isCurrentPlayer() == true && playerO.isCurrentPlayer() == false) {
+                playerX.unsetCurrentPlayer()
+                playerO.setCurrentPlayer()
+            } else {
+                playerO.unsetCurrentPlayer()
+                playerX.setCurrentPlayer()
+            }
+            setPlayer()
+        }
+        const promptMove = () => { // Temporary for Console Version
+            do {
+                move = window.prompt("Enter the cell you'd like to place your marker (1 - 9)")
+            } while (game.getGameBoardAtIndex(move) !== undefined)
+        }
+        const handleMove = () => {
+            game.setGameBoard(move, currentSign)
+        }
+        const setPlayer = () => {
+            if (playerX.isCurrentPlayer()) currentSign = playerX.getSign()
+            else if (playerO.isCurrentPlayer()) currentSign = playerO.getSign()
+            else currentSign = null
+        }
+        return {
+            newTurn,
+            promptMove,
+            handleMove
+        }
+    })()
+
+    const checkBoard = (() => {
+        const checkWin = (sign) => {
+            gameBoard = game.getGameBoard()
+            if (checkRows(gameBoard, sign) || checkColumns(gameBoard, sign) || checkDiagonals(gameBoard, sign)) {
+                gameResult = 'Win'
+                isGameOver = true
+            }
+        }
+        const checkDraw = () => {
+            gameBoard = game.getGameBoard()
+            if (gameBoard.every(cell => cell !== '?')) {
+                gameResult = 'Tie'
+                isGameOver = true
+            }
+        }
+        const checkRows = (board, sign) => {
+            gameBoard = board
+            if (gameBoard[0] == sign && gameBoard[1] == sign && gameBoard[2] == sign
+                || gameBoard[3] == sign && gameBoard[4] == sign && gameBoard[5] == sign
+                || gameBoard[6] == sign && gameBoard[7] == sign && gameBoard[8] == sign)
+            return true
+            else return false
+        }
+        const checkColumns = (board, sign) => {
+            gameBoard = board
+            if (gameBoard[0] == sign && gameBoard[3] == sign && gameBoard[6] == sign
+                || gameBoard[1] == sign && gameBoard[4] == sign && gameBoard[7] == sign
+                || gameBoard[2] == sign && gameBoard[5] == sign && gameBoard[8] == sign)
+            return true
+            else return false
+        }
+        const checkDiagonals = (board, sign) => {
+            gameBoard = board
+            if (gameBoard[0] == sign && gameBoard[4] == sign && gameBoard[8] == sign
+                || gameBoard[2] == sign && gameBoard[4] == sign && gameBoard[6] == sign) 
+            return true
+            else return false
+        }
+        return {
+            checkWin,
+            checkDraw,
+        }
+    })()
 
     return {
         newGame,
