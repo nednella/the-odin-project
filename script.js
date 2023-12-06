a6548cf73180e333148f98fa214e2b70ff8810b0
nednella <119449811+nednella@users.noreply.github.com>
2023-12-06 19:04:47 +0000

    Reshuffle JS file structure and rename module

--- script.js
+++ script.js
@@ -1,5 +1,214 @@
 // JavaScript
 
+const game = (() => {
+    let gameBoard = Array(9).fill(undefined)
+
+    const resetGameBoard = () => gameBoard.forEach((element, index) => gameBoard[index] = undefined)
+    const setGameBoard = (i, sign) => gameBoard[i - 1] = sign
+    const getGameBoard = () => gameBoard
+    const getGameBoardAtIndex = (i) => gameBoard[i - 1]
+    const getGameBoardEmptyFields = () => {
+        let emptyFields = []
+
+        gameBoard.forEach((element, index) => {
+            if (element == undefined) {
+                emptyFields.push(index + 1)
+            }
+        })
+
+        return emptyFields
+    }
+    const checkGameBoard = (sign) => {
+        if (checkWin(sign)) return 'win'
+        else if (checkTie()) return 'tie'
+        else return null
+    }
+    const checkWin = (sign) => {
+        return false
+            // Rows
+            || (gameBoard[0] == sign && gameBoard[1] == sign && gameBoard[2] == sign)
+            || (gameBoard[3] == sign && gameBoard[4] == sign && gameBoard[5] == sign)
+            || (gameBoard[6] == sign && gameBoard[7] == sign && gameBoard[8] == sign)
+            // Columns
+            || (gameBoard[0] == sign && gameBoard[3] == sign && gameBoard[6] == sign)
+            || (gameBoard[1] == sign && gameBoard[4] == sign && gameBoard[7] == sign)
+            || (gameBoard[2] == sign && gameBoard[5] == sign && gameBoard[8] == sign)
+            // Diagonals
+            || (gameBoard[0] == sign && gameBoard[4] == sign && gameBoard[8] == sign)
+            || (gameBoard[2] == sign && gameBoard[4] == sign && gameBoard[6] == sign)
+    }
+    const checkTie = () => {
+        return !gameBoard.includes(undefined)
+    }
+
+    return {
+        resetGameBoard,
+        setGameBoard,
+        getGameBoard,
+        getGameBoardAtIndex,
+        getGameBoardEmptyFields,
+        checkGameBoard,
+    }
+})()
+
+const player = (sign, icon, isHuman, aiMode) => {
+    let player_Name = `Player ${sign}`
+    let player_Sign = icon
+    let player_isHuman = isHuman
+    let ai_difficulty = aiMode
+
+    const getPlayerName = () => { return player_Name }
+    const getPlayerSign = () => { return player_Sign }
+    const isPlayerHuman = () => { return player_isHuman }
+    const isAiDifficulty = () => { return ai_difficulty }
+
+    return {
+        name: getPlayerName,
+        sign: getPlayerSign,
+        isHuman: isPlayerHuman,
+        difficulty: isAiDifficulty,
+    }
+}
+
+const defaultConfig = (() => {
+    const playerX = player('X', '❤️', false, true, )
+    const playerO = player('O', '💚', false, false, 'impossible')
+    return {
+        playerX,
+        playerO,
+    }
+})()
+
+const gameController = (() => {
+    const playerX = defaultConfig.playerX
+    const playerO = defaultConfig.playerO
+    let currentPlayer, opponentPlayer, currentRound, isGameOver, gameStatus
+
+    const newGame = () => {
+        console.clear()
+        console.log('%cWelcome to JS Tic Tac Toe (Browser Console Version)', 'font-size: x-large')
+        game.displayGameBoard()
+        currentRound = 0
+        do {
+            currentRound++
+            playRound()
+        } while (!isGameOver)
+        gameOver()
+    }
+
+    const resetGame = () => {
+        currentPlayer = ''
+        opponentPlayer = ''
+        isGameOver  = ''
+        gameStatus = ''
+        game.resetGameBoard()
+        playerX.unsetCurrent()
+        playerO.unsetCurrent()
+    }
+
+    const playRound = () => {
+        gameLogic.newTurn()
+        gameLogic.handleMove()
+        gameLogic.checkBoard()
+        if (gameStatus !== 'quit') {
+            console.log('%cCurrent Round: ' + currentRound, 'font-weight: bold')
+            game.displayGameBoard()    
+        }
+    }
+
+    const gameOver = () => {
+        console.log(`%cGame Over...`, 'font-size: x-large')
+         if (gameStatus == 'win') {
+            console.log(`%c${currentPlayer.name()} (${currentPlayer.sign()}) Wins!`, 'font-size: x-large')
+        } else if (gameStatus == 'tie') {
+            console.log(`%cIt's a Tie!`, 'font-size: x-large')
+        } else console.log(`%cGame has been cancelled.`, 'font-size: x-large')
+        resetGame()
+    }
+
+    const gameLogic = (() => {
+        const newTurn = () => {
+            if (currentRound == 0) {
+                playerX.setCurrent()
+            }
+            if (playerX.isCurrent()) { playerX.unsetCurrent(), playerO.setCurrent() } 
+            else { playerO.unsetCurrent(), playerX.setCurrent() }
+            helperFns.setPlayer()
+        }
+
+        const handleMove = () => {
+            if (currentPlayer.isHuman()) {
+                do {
+                    move = window.prompt(`Enter where you'd like to place your marker (1 - 9) ["C" to Quit]`)
+                    if ( move == 'C') {
+                        helperFns.setGameStatus('quit')
+                        break
+                    }
+                } while (game.getGameBoardAtIndex(move) !== undefined)       
+            } else {
+                move = aiLogic.findMove(currentPlayer, opponentPlayer)
+            }
+            
+            game.setGameBoard(move, currentPlayer.sign())
+        }
+
+        const checkBoard = () => {
+            if (helperFns.checkWin(currentPlayer.sign())) helperFns.setGameStatus('win')
+            else if (currentRound == 9) helperFns.setGameStatus('tie')
+        }
+
+        const helperFns = (() => {
+            const checkWin = (sign) => {
+                gameBoard = game.getGameBoard()
+                return false
+                    // Rows
+                    || (gameBoard[0] == sign && gameBoard[1] == sign && gameBoard[2] == sign)
+                    || (gameBoard[3] == sign && gameBoard[4] == sign && gameBoard[5] == sign)
+                    || (gameBoard[6] == sign && gameBoard[7] == sign && gameBoard[8] == sign)
+                    // Columns
+                    || (gameBoard[0] == sign && gameBoard[3] == sign && gameBoard[6] == sign)
+                    || (gameBoard[1] == sign && gameBoard[4] == sign && gameBoard[7] == sign)
+                    || (gameBoard[2] == sign && gameBoard[5] == sign && gameBoard[8] == sign)
+                    // Diagonals
+                    || (gameBoard[0] == sign && gameBoard[4] == sign && gameBoard[8] == sign)
+                    || (gameBoard[2] == sign && gameBoard[4] == sign && gameBoard[6] == sign)
+            }
+    
+            const setPlayer = () => {
+                if (playerX.isCurrent()) { currentPlayer = playerX, opponentPlayer = playerO }
+                else  { currentPlayer = playerO, opponentPlayer = playerX }
+            }
+    
+            const setGameStatus = (result) => {
+                gameStatus = result
+                setGameOver()  
+            }
+    
+            const setGameOver = () => {
+                return isGameOver = true
+            }
+
+            return {
+                checkWin,
+                setPlayer,
+                setGameStatus
+            }
+        })()
+
+        return {
+            newTurn,
+            handleMove,
+            checkBoard,
+        }
+
+    })()
+
+    return {
+        newGame,
+        resetGame,
+    }
+})()
+
 const displayController = (() => {
     titleContainer = document.getElementById('title-container')
     gameContainer = document.getElementById('game-container')
@@ -45,11 +254,18 @@ const displayController = (() => {
         restartButton.addEventListener('click', () => tmpFnName.newGame())      // Restart
 
         gridItems.forEach(item => {                                             // Handle Move
-        item.addEventListener('click', () => {
-            console.log(item.dataset.gridPos)
+            item.addEventListener('click', () => {
+                
+                move = item.dataset.gridPos
+                console.log(move)
 
-            // TODO: Implement game integration using gridPos dataset values
-
+                if (selectedGameMode == 1) {
+                    gameController.playRound(move)           // Human round
+                    // TODO: add delay between rounds played
+                    gameController.playRound()               // AI round
+                } else {
+                    gameController.playRound(move)
+                }
             })
         })
     })()
@@ -60,7 +276,7 @@ const displayController = (() => {
 
     const renderGameBoard = (gameBoard) => {
         for (i = 0; i < gridItems.length; i++) {
-            if (gameBoard[i] !== undefined) {
+            if (gameBoard[i] !== undefined) { // combine into 1 if statement
                 if (gridItems[i] !== '') {
                     gridItems[i].innerText = gameBoard[i]    
                 }
@@ -95,7 +311,7 @@ const displayController = (() => {
             ui.clearScorecards()
 
             /* Game Resets */
-            ttt.resetGame()
+            gameController.resetGame()
         }
 
         const setGameMode = (input) => {
@@ -199,73 +415,13 @@ const displayController = (() => {
             clearScorecards,
         }
     })()
-})()
-
-const game = (() => {
-    let gameBoard = Array(9).fill(undefined)
-
-    const resetGameBoard = () => gameBoard.forEach((element, index) => gameBoard[index] = undefined)
-    const setGameBoard = (i, sign) => gameBoard[i - 1] = sign
-    const getGameBoard = () => gameBoard
-    const getGameBoardAtIndex = (i) => gameBoard[i - 1]
-    const getGameBoardEmptyFields = () => {
-        let emptyFields = []
-
-        gameBoard.forEach((element, index) => {
-            if (element == undefined) {
-                emptyFields.push(index + 1)
-            }
-        })
-
-        return emptyFields
-    }
-    const displayGameBoard = () => {
-        let tmp = ''
-        tmp += (' ' + '\n')
-        tmp += (' ' + (gameBoard[0] || '1 ') + ' | ' + (gameBoard[1] || ' 2 ') + ' | ' + (gameBoard[2] || ' 3 ') + '\n')
-        tmp += ('---------------' + '\n')
-        tmp += (' ' + (gameBoard[3] || '4 ') + ' | ' + (gameBoard[4] || ' 5 ') + ' | ' + (gameBoard[5] || ' 6 ') + '\n')
-        tmp += ('---------------'  + '\n')
-        tmp += (' ' + (gameBoard[6] || '7 ') + ' | ' + (gameBoard[7] || ' 8 ') + ' | ' + (gameBoard[8] || ' 9 ') + '\n')
-        tmp += (' ')
-        console.log(tmp)
-    }
 
     return {
-        resetGameBoard,
-        setGameBoard,
-        getGameBoard,
-        getGameBoardAtIndex,
-        getGameBoardEmptyFields,
-        displayGameBoard,
+        renderGameBoard,
+        renderGameMessage,
+        renderScorecards,
     }
 })()
-
-const player = (sign, icon, isCurrent, isHuman, aiMode) => {
-    let player_Name = `Player ${sign}`
-    let player_Sign = icon
-    let player_isCurrent = isCurrent
-    let player_isHuman = isHuman
-    let ai_difficulty = aiMode
-
-    const getPlayerName = () => { return player_Name }
-    const getPlayerSign = () => { return player_Sign }
-    const isPlayerCurrent = () => { return player_isCurrent }
-    const setPlayerCurrent = () => { return player_isCurrent = true }
-    const unsetPlayerCurrent = () => { return player_isCurrent = false }
-    const isPlayerHuman = () => { return player_isHuman }
-    const isAiDifficulty = () => { return ai_difficulty }
-
-    return {
-        name: getPlayerName,
-        sign: getPlayerSign,
-        isCurrent: isPlayerCurrent,
-        setCurrent: setPlayerCurrent,
-        unsetCurrent: unsetPlayerCurrent,
-        isHuman: isPlayerHuman,
-        difficulty: isAiDifficulty,
-    }
-}
 
 const aiLogic = (() => {
 
@@ -489,146 +645,5 @@ const aiLogic = (() => {
 
     return {
         findMove,
-    }
-})()
-
-const defaultConfig = (() => {
-    const playerX = player('X', '❤️', false, true, )
-    const playerO = player('O', '💚', false, false, 'impossible')
-    return {
-        playerX,
-        playerO,
-    }
-})()
-
-const ttt = (() => {
-    console.log('%cTo play a game, type "ttt.newGame()"', 'font-size: x-large')
-
-    const playerX = defaultConfig.playerX
-    const playerO = defaultConfig.playerO
-    let currentPlayer, opponentPlayer, currentRound, isGameOver, gameStatus
-
-    const newGame = () => {
-        console.clear()
-        console.log('%cWelcome to JS Tic Tac Toe (Browser Console Version)', 'font-size: x-large')
-        game.displayGameBoard()
-        currentRound = 0
-        do {
-            currentRound++
-            playRound()
-        } while (!isGameOver)
-        gameOver()
-    }
-
-    const resetGame = () => {
-        currentPlayer = ''
-        opponentPlayer = ''
-        isGameOver  = ''
-        gameStatus = ''
-        game.resetGameBoard()
-        playerX.unsetCurrent()
-        playerO.unsetCurrent()
-    }
-
-    const playRound = () => {
-        gameLogic.newTurn()
-        gameLogic.handleMove()
-        gameLogic.checkBoard()
-        if (gameStatus !== 'quit') {
-            console.log('%cCurrent Round: ' + currentRound, 'font-weight: bold')
-            game.displayGameBoard()    
-        }
-    }
-
-    const gameOver = () => {
-        console.log(`%cGame Over...`, 'font-size: x-large')
-         if (gameStatus == 'win') {
-            console.log(`%c${currentPlayer.name()} (${currentPlayer.sign()}) Wins!`, 'font-size: x-large')
-        } else if (gameStatus == 'tie') {
-            console.log(`%cIt's a Tie!`, 'font-size: x-large')
-        } else console.log(`%cGame has been cancelled.`, 'font-size: x-large')
-        resetGame()
-    }
-
-    const gameLogic = (() => {
-        const newTurn = () => {
-            if (currentRound == 0) {
-                playerX.setCurrent()
-            }
-            if (playerX.isCurrent()) { playerX.unsetCurrent(), playerO.setCurrent() } 
-            else { playerO.unsetCurrent(), playerX.setCurrent() }
-            helperFns.setPlayer()
-        }
-
-        const handleMove = () => {
-            if (currentPlayer.isHuman()) {
-                do {
-                    move = window.prompt(`Enter where you'd like to place your marker (1 - 9) ["C" to Quit]`)
-                    if ( move == 'C') {
-                        helperFns.setGameStatus('quit')
-                        break
-                    }
-                } while (game.getGameBoardAtIndex(move) !== undefined)       
-            } else {
-                move = aiLogic.findMove(currentPlayer, opponentPlayer)
-            }
-            
-            game.setGameBoard(move, currentPlayer.sign())
-        }
-
-        const checkBoard = () => {
-            if (helperFns.checkWin(currentPlayer.sign())) helperFns.setGameStatus('win')
-            else if (currentRound == 9) helperFns.setGameStatus('tie')
-        }
-
-        const helperFns = (() => {
-            const checkWin = (sign) => {
-                gameBoard = game.getGameBoard()
-                return false
-                    // Rows
-                    || (gameBoard[0] == sign && gameBoard[1] == sign && gameBoard[2] == sign)
-                    || (gameBoard[3] == sign && gameBoard[4] == sign && gameBoard[5] == sign)
-                    || (gameBoard[6] == sign && gameBoard[7] == sign && gameBoard[8] == sign)
-                    // Columns
-                    || (gameBoard[0] == sign && gameBoard[3] == sign && gameBoard[6] == sign)
-                    || (gameBoard[1] == sign && gameBoard[4] == sign && gameBoard[7] == sign)
-                    || (gameBoard[2] == sign && gameBoard[5] == sign && gameBoard[8] == sign)
-                    // Diagonals
-                    || (gameBoard[0] == sign && gameBoard[4] == sign && gameBoard[8] == sign)
-                    || (gameBoard[2] == sign && gameBoard[4] == sign && gameBoard[6] == sign)
-            }
-    
-            const setPlayer = () => {
-                if (playerX.isCurrent()) { currentPlayer = playerX, opponentPlayer = playerO }
-                else  { currentPlayer = playerO, opponentPlayer = playerX }
-            }
-    
-            const setGameStatus = (result) => {
-                gameStatus = result
-                setGameOver()  
-            }
-    
-            const setGameOver = () => {
-                return isGameOver = true
-            }
-
-            return {
-                checkWin,
-                setPlayer,
-                setGameStatus
-            }
-        })()
-
-        return {
-            newTurn,
-            handleMove,
-            checkBoard,
-        }
-
-    })()
-
-    return {
-        newGame,
-        resetGame,
     }
 })()
