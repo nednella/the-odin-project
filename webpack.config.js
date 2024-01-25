6ac5229cb1c2f497420a078fbf6d7e256ec4d89a
nednella <119449811+nednella@users.noreply.github.com>
2024-01-25 22:25:30 +0000

    Install html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            title: 'The Odin Project: Restaurant Page',
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
