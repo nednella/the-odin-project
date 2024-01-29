da59e50f81f2ae0ba11a9826898d77adbc6650b5
nednella <119449811+nednella@users.noreply.github.com>
2024-01-29 19:13:45 +0000

    Fix webpack file naming error

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,9 @@ module.exports = {
         }),
     ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     optimization: {
         runtimeChunk: 'single',
