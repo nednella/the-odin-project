9a449eb5f9ba31293439b5b3b14cfefe8e63c1af
nednella <119449811+nednella@users.noreply.github.com>
2024-04-05 21:31:28 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            title: 'Spotify Clone Application',
+        }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.(scss|css)$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
