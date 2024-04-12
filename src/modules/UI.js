3afebf3c5e9383dc91b1f799ce012051ecd2ad87
nednella <119449811+nednella@users.noreply.github.com>
2024-04-12 01:11:25 +0100

    Add: API call connection to dashboard, display location info

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,5 +1,6 @@
+import API from './API.js'
 import searchBar from './searchBar.js'
-import { createElement } from './Utilities.js'
+import { createElement, parseDate } from './Utilities.js'
 
 export default class UI {
     constructor() {
@@ -9,29 +10,110 @@ export default class UI {
     }
 
     static initApp() {
-        console.log('UI: App initialising')
-        UI.renderHome()
+        UI.renderDashboard()
     }
 
-    static initEventListeners() {
+    static #initEventListeners() {
         return
     }
 
-    static clear() {
-        console.log('Clearing UI')
-        document.getElementById('content').textContent = ''
+    static #clear() {
+        document.querySelector('.location > .name').textContent = ''
+        document.querySelector('.location > .region').textContent = ''
+        document.querySelector('.location > .country').textContent = ''
+        document.querySelector('.local-time').textContent = ''
+    }
+
+    static #populateHeader() {
+        const header = document.querySelector('header')
+        if (header.textContent) return
+
+        header.append(
+            createElement('div', { classList: 'logo', textContent: 'Logo' }),
+            createElement('div', { classList: 'unit-toggle' }),
+            new searchBar().getSearchBar()
+        )
+        header
+            .querySelector('.unit-toggle')
+            .append(
+                createElement('span', { classList: 'active', textContent: '\u00B0C' }),
+                createElement('span', { classList: '', textContent: '|' }),
+                createElement('span', { classList: '', textContent: '\u00B0F' })
+            )
+    }
+
+    static #populateHome() {
+        document.querySelector('section.homepage').append(new searchBar('70%').getSearchBar())
+    }
+
+    static #populateDashboard(data) {
+        this.#populateHeader()
+
+        API.forecast('Newcastle Upon Tyne').then((data) => {
+            console.log(data)
+            const location = data[0].location
+            const current = data[1].current
+            const forecast = data[2].forecast
+
+            // Debugging
+            console.log(location, current, forecast)
+            for (let property in location) {
+                console.log(location[property])
+            }
+
+            // Location
+            document.querySelector('.location > .name').textContent = location['name']
+
+            // Region
+            if (location['name'] !== location['region'])
+                document.querySelector('.location > .region').textContent = location['region']
+
+            // Country
+            document.querySelector('.location > .country').textContent = location['country']
+
+            // Local Time
+            document.querySelector('.local-time').textContent = parseDate(
+                new Date().toLocaleString('en-GB', {
+                    timeZone: location['tz_id'],
+                })
+            )
+
+            // Local Stats
+            // TODO
+
+            // Current Weather
+            // TODO
+
+            // Forecasted Weather
+            // TODO
+        })
+    }
+
+    static #displaySection(section) {
+        document.querySelector('#grid-container').className = ''
+        document
+            .querySelectorAll('section')
+            .forEach((section) => section.classList.remove('active'))
+
+        if (section == 'homepage') {
+            document.querySelector('#grid-container').classList.add('homepage--active')
+            document.querySelector('section.homepage').classList.add('active')
+        }
+        if (section == 'forecast') {
+            document.querySelector('#grid-container').classList.add('dashboard--active')
+            document.querySelector('section.dashboard').classList.add('active')
+        }
     }
 
     static renderHome() {
-        UI.clear()
+        UI.#clear()
+        UI.#populateHome()
+        UI.#displaySection('homepage')
+    }
 
-        const content = document.getElementById('content')
-        content.append(
-            createElement('div', {
-                classList: 'heading',
-                textContent: 'Weather App',
-            }),
-            new searchBar('70%').getSearchBar()
-        )
+    static renderDashboard(data) {
+        UI.#clear()
+        UI.#populateDashboard(data)
+        UI.#displaySection('forecast')
     }
 }
--- src/modules/Utilities.js
+++ src/modules/Utilities.js
@@ -1,4 +1,14 @@
-// Utility functions
+import { format } from 'date-fns'
 
+// Utility functions
 export const createElement = (type, properties = {}) =>
     Object.assign(document.createElement(type), properties)
+
+export const parseDate = (date) => {
+    let parts = date.split(/[/,]/)
+    date = format(
+        `${parts[2]}-${parts[1]}-${parts[0]}${parts[3]}`,
+        "EEEE, d MMMM yyyy '| Local Time: 'hh:mm aa"
+    )
+    return date
+}
